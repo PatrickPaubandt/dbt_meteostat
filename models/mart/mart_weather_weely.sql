218d7190c17fb1c63d43910f4de342bcf5bb4150
Patrick Paubandt <patrick.paubandt.edu@gmail.com>
2025-12-17 09:59:04 +0100

    round some Vars from mart weather weekly

--- models/mart/mart_weather_weely.sql
+++ models/mart/mart_weather_weely.sql
@@ -1,16 +1,16 @@
 SELECT
     airport_code,
     cw,
-    AVG(avg_temp_c) AS avg_week_temp,
+    Round(AVG(avg_temp_c),1) AS avg_week_temp,
     MIN(min_temp_c) AS min_week_temp,
     MAX(max_temp_c) AS max_week_temp,
     SUM(precipitation_mm) AS sum_week_precipitation,
     SUM(max_snow_mm) AS sum_week_snowmm,
     AVG(avg_wind_direction) AS avg_week_winddirection,
-    AVG(avg_wind_speed_kmh) AS avg_week_windspeed,
+    ROUND(AVG(avg_wind_speed_kmh),1) AS avg_week_windspeed,
     AVG(wind_peakgust_kmh) AS avg_week_peakgust,
     MAX(wind_peakgust_kmh) AS max_week_peakgust,
-    AVG(avg_pressure_hpa) AS avg_week_pressurehpa,
+    ROUND(AVG(avg_pressure_hpa),1) AS avg_week_pressurehpa,
     AVG(sun_minutes) AS avg_week_sunminutes,
     SUM(sun_minutes) AS sum_week_sunminutes
 FROM {{ ref('prep_weather_daily') }}
