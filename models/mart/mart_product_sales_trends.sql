ba56e256fe6b36a1a459f8975cf484a21f23579b
Patrick Paubandt <patrick.paubandt.edu@gmail.com>
2025-12-17 14:39:14 +0100

    joiwf

--- models/mart/mart_product_sales_trends.sql
+++ models/mart/mart_product_sales_trends.sql
@@ -25,7 +25,6 @@ mom_calc AS (
         product_id,
         product_name,
         total_revenue,
-
         LAG(total_revenue) OVER (
             PARTITION BY product_id
             ORDER BY order_year, order_month
@@ -34,25 +33,22 @@ mom_calc AS (
 )
 
 SELECT
-    order_year,
-    order_month,
     product_id,
     product_name,
+    order_year,
+    order_month,
     total_revenue,
 
     CASE
-        WHEN prev_month_revenue IS NULL THEN NULL
-        WHEN prev_month_revenue = 0 THEN NULL
-        ELSE
-            (total_revenue - prev_month_revenue) * 100.0
-            / prev_month_revenue
+        WHEN prev_month_revenue IS NULL OR prev_month_revenue = 0 THEN NULL
+        ELSE ROUND((total_revenue - prev_month_revenue) * 100.0 / prev_month_revenue, 2)
     END AS revenue_change_pct_mom,
 
     CASE
-        WHEN prev_month_revenue IS NULL THEN 'no sales prev month'
-        WHEN prev_month_revenue = 0 THEN 'no sales prev month'
+        WHEN prev_month_revenue IS NULL OR prev_month_revenue = 0 THEN 'no sales prev month'
         ELSE 'ok'
     END AS mom_status
 
 FROM mom_calc
 ORDER BY product_name, order_year, order_month
+
